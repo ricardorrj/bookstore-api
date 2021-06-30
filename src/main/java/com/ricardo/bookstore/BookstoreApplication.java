70cb1c3524dc8becfeb3bfe7cf3138cb36c1593c
Ricardo Jr <ricardorrj25@gmail.com>
2021-06-29 22:29:30 -0300

    Criado as interfaces repositorios / Teste comunicação com bd H2

--- src/main/java/com/ricardo/bookstore/BookstoreApplication.java
+++ src/main/java/com/ricardo/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.ricardo.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.ricardo.bookstore.domain.Categoria;
+import com.ricardo.bookstore.domain.Livro;
+import com.ricardo.bookstore.repositories.CategoriaRepository;
+import com.ricardo.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository catRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat = new Categoria(null, "Curso Java", "TI");
+		Livro livro = new Livro(null, "Titulo Livro", "Autor", "Texto teste", cat);
+		
+		cat.getLivros().addAll(Arrays.asList(livro));
+		
+		this.catRepository.saveAll(Arrays.asList(cat));
+		this.livroRepository.saveAll(Arrays.asList(livro));
+	}
+	
 }
