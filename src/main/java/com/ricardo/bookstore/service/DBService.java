2b97c790aad230a3943d20bb401b07800e365fd4
Ricardo Jr <ricardorrj25@gmail.com>
2021-06-29 22:42:14 -0300

    perfil de teste configurado

---
+++ src/main/java/com/ricardo/bookstore/service/DBService.java
@@ -0,0 +1,37 @@
+package com.ricardo.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardo.bookstore.domain.Categoria;
+import com.ricardo.bookstore.domain.Livro;
+import com.ricardo.bookstore.repositories.CategoriaRepository;
+import com.ricardo.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository catRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public void instanciaBaseDataBase() {
+		Categoria cat = new Categoria(null, "Curso Java", "TI");
+		Categoria cat2 = new Categoria(null, "Programação POO", "TI");
+		Categoria cat3 = new Categoria(null, "Desenvolvimento Web", "TI");
+		
+		Livro livro = new Livro(null, "Titulo Livro", "Autor", "Texto teste", cat);
+		Livro livro2 = new Livro(null, "Titulo Livro2", "Autor2", "Texto teste2", cat);
+		Livro livro3 = new Livro(null, "Titulo Livro3", "Autor3", "Texto teste3", cat2);
+		Livro livro4 = new Livro(null, "Titulo Livro4", "Autor4", "Texto teste4", cat3);
+		
+		cat.getLivros().addAll(Arrays.asList(livro));
+		
+		this.catRepository.saveAll(Arrays.asList(cat, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(livro, livro2, livro3, livro4));
+	}
+}
